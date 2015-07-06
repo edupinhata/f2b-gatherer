27676236882aa84b8e8dddf27544c16328414ada
Eduardo Pinhata <edupinhata@gmail.com>
2015-07-06 23:25:29 +0000

    First version of server.java This is just the first scratch

---
+++ server.java
@@ -0,0 +1,17 @@
+/*Server file
+*Will keep running and reading the f2b log.
+*
+*Author: Eduardo Pinhata
+*Init Date: 07/07/2015
+*/
+
+
+public class server{
+
+
+	public static void main(String [] args){
+
+	}
+
+}
+
