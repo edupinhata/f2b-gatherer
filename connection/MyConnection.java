f1c7790455f31078612ab96a10a72e97a366e095
Eduardo Pinhata <epinhata@Eduardos-MacBook-Pro.local>
2015-07-23 19:54:31 -0500

    Class that controll the access to databas

---
+++ connection/MyConnection.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MyConnection {
+    /* Singleton */
+
+    private static MyConnection singleton = null;
+
+    public static MyConnection getInstance() {
+        try {
+            /* Lazy Initialization */
+            if (singleton == null || singleton.sqlConnection.isClosed()) {
+                singleton = new MyConnection();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return singleton;
+    }
+    public Connection sqlConnection;
+
+    private MyConnection() {
+        try {
+            //Class.forName("org.sqlite.JDBC");
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            String textoConexao = "jdbc:mysql://localhost:8889/VIET_TIEN_SIMPLE?user=root&password=root";
+            //String textoConexao = "jdbc:mysql://172.17.44.187/bolao_copa?user=root&password=root";
+            sqlConnection = DriverManager.getConnection(textoConexao);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
